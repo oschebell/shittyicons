27d9584b818760a8a92438231bffb2e119a2b754
Owen Schebella <owenschebella@Owens-MacBook.local>
2018-05-22 23:51:40 +1000

    Got icon images to load

--- ninja-smoothies/src/components/Index.vue
+++ ninja-smoothies/src/components/Index.vue
@@ -1,16 +1,9 @@
 <template>
   <div class="index container">
-    <div class="card" v-for="smoothie in smoothies" :key="smoothie.id">
+    <div class="card" v-for="icon in icons" :key="icon.id">
       <div class="card-content">
-        <i class="material-icons delete" @click="deleteSmoothie(smoothie.id)">delete</i>
-        <h2 class="indigo-text">{{ smoothie.title }}</h2>
-        <ul class="ingredients">
-          <li v-for="(ing, index) in smoothie.ingredients" :key="index">
-            <span class="chip">{{ ing }}</span>
-          </li>
-        </ul>
-          <a :href="smoothie.image" download="smoothie.image"><img :src="smoothie.image"  style="height:48px"  alt="" ></a>
-       
+          <a :href="icon.image" download="icon.image"><img :src="icon.image"  style="height:48px"  alt="" ></a>
+          <p>{{ icon.title }}</p>
       </div>
     </div>
   </div>
@@ -23,23 +16,39 @@ export default {
   name: 'Index',
   data(){
     return{
-      smoothies: []
+      // smoothies: [],
+      icons:[]
     }
   },
   methods: {
-    deleteSmoothie(id){
-      this.smoothies = this.smoothies.filter(smoothie => {
-        return smoothie.id != id
+  //   deleteSmoothie(id){
+  //     this.smoothies = this.smoothies.filter(smoothie => {
+  //       return smoothie.id != id
+  //     })
+  //   }
+  // }, 
+   deleteicon(id){
+      this.icons = this.icons.filter(icon => {
+        return icon.id != id
       })
     }
   }, 
   created(){
     
     //fetch data from firestore
-    db.collection('Smoothies').get()
+    // db.collection('Smoothies').get()
+    // .then(snapshot => {
+    //   snapshot.forEach(doc => {
+    //     this.smoothies.push(doc.data())
+    //   })
+    // })
+    // .catch(err => {
+    //   console.log(err)
+    // })
+      db.collection('icons').get()
     .then(snapshot => {
       snapshot.forEach(doc => {
-        this.smoothies.push(doc.data())
+        this.icons.push(doc.data())
       })
     })
     .catch(err => {
@@ -52,7 +61,7 @@ export default {
 <style>
 .index{
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr;
   grid-gap: 30px;
   margin-top: 60px;
 }
@@ -74,5 +83,15 @@ export default {
   cursor: pointer;
   color: #aaa;
   font-size: 1.4em;
+}
+
+.card p {
+  text-align: center; 
+}
+
+img {
+	margin-left: auto;
+	margin-right: auto;
+	display: block;
 }
 </style>
--- ninja-smoothies/src/components/Navbar.vue
+++ ninja-smoothies/src/components/Navbar.vue
@@ -1,15 +1,10 @@
 <template>
   <div class="navbar">
-    <nav class="nav-extended indigo darken-2">
+    <nav class="nav-extended">
       <div class="nav-content">
         <router-link :to="{ name: 'Index' }">
-          <span class="nav-title">Ninja Smoothies </span>
+          <span class="nav-title">shitty icons</span>
         </router-link>
-        <a href="" class="btn-floating btn-large halfway-fab pink">
-          <router-link to="">
-            <i class="material-icons">add</i>
-          </router-link>
-        </a>
       </div>
     </nav>
   </div>
@@ -27,8 +22,31 @@ export default {
 </script>
 
 <style>
+
+.nav-title {
+  color: #000;
+}
 .navbar nav{
   padding: 0 20px;
+  background-color: #fff;
+  box-shadow: none;
 }
+
+.index-container {
+  width: 500px;
+}
+.card {
+  box-shadow: none;
+}
+
+.card-content a {
+  align-content: center;
+}
+
+a img {
+  align-content: center;
+}
+
+
 </style>
 
